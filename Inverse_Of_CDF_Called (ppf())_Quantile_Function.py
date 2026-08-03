30f93c3c13a7d45d74ddeb6d2fc098978925a467
t38748845-maker <t38748845@gmail.com>
2026-08-03 14:02:20 +0530

    Update Inverse_Of_CDF_Called (ppf())_Quantile_Function.py

--- Inverse_Of_CDF_Called (ppf())_Quantile_Function.py
+++ Inverse_Of_CDF_Called (ppf())_Quantile_Function.py
@@ -1,1 +1,9 @@
-#
+# using the inverse CDF(called ppf()) in python...
+# importing norm(normal) from scipy.stats
+from scipy.stats import norm
+
+# define formula for inverse of CDF is ppf()
+x = norm.ppf(0.95, loc=64.43, scale=2.99)
+
+# print value of x...
+print(x)
