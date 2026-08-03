6f3023dc1708ddf0a37e04a4b804499ef33f05cf
t38748845-maker <t38748845@gmail.com>
2026-08-03 14:06:38 +0530

    Update Random_Number_Generator.py

--- Random_Number_Generator.py
+++ Random_Number_Generator.py
@@ -1,1 +1,13 @@
-#
+# generating random number from normal distribution... 
+
+# importing random...
+import random
+
+# importing norm(normal) from scipy.stats...
+from scipy.stats import norm
+
+# applies For-Loop...
+for i in range(0, 1000):
+    random_p = random.uniform(0.0, 1.0)
+    random_weight = norm.ppf(random_p, loc=64.43, scale=2.99)
+    print(random_weight)
